acd52eee28598c77dd1096fcc995391bed017eb5
Alban Rudelle <alban@MacBook-Air-de-Alban.local>
2026-06-16 00:26:07 +0200

    fix: fix schemaTypes index module export

---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,6 @@
+import {temoignage} from './temoignage'
+import {disponibilite} from './disponibilite'
+import {photo} from './photo'
+import {page} from './page'
+
+export const schema = {types: [temoignage, disponibilite, photo, page]}
