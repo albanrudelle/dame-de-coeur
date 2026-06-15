0f79f83b753b28d5e74fce39788870ac02b458a2
Alban Rudelle <alban@MacBook-Air-de-Alban.local>
2026-06-16 00:35:28 +0200

    fix: replace next-sanity/live with basic sanity fetch

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -1,9 +1,13 @@
-// Querying with "sanityFetch" will keep content automatically updated
-// Before using it, import and render "<SanityLive />" in your layout, see
-// https://github.com/sanity-io/next-sanity#live-content-api for more information.
-import { defineLive } from "next-sanity/live";
-import { client } from './client'
+import { createClient } from 'next-sanity'
+import { apiVersion, dataset, projectId } from '../env'
 
-export const { sanityFetch, SanityLive } = defineLive({
-  client,
-});
+export const client = createClient({
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: true,
+})
+
+export async function sanityFetch<T>(query: string, params = {}) {
+  return client.fetch<T>(query, params)
+}
